089d685ce996b33e52f397f8a0c028a6e3fd323c
anderson rafael <franciscoandersonrafael@gmail.com>
2024-02-21 23:30:57 -0300

    estrutura de repetição while

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ function tocar_play() {
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
+let contador = 0;
+//enquanto
+while (contador < 8) {
+    listaDeTeclas[contador].onclick = tocar_play;
+    contador = contador + 1;
+    console.log(contador);
+}
+
+
+
