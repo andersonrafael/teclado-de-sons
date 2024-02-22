52c57085c2dd2dfcbfa62f6e3501d942da8e509f
anderson rafael <franciscoandersonrafael@gmail.com>
2024-02-21 23:09:45 -0300

    aplicando a lista de teclas const listaDeTeclas

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ function tocar_play() {
     document.querySelector('#som_tecla_clap').play();
 }
 
-document.querySelectorAll('.tecla').onclick = tocar_play;
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
