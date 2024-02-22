1c05aee2b2ce074360d9af96d1944969ac248f96
anderson rafael <franciscoandersonrafael@gmail.com>
2024-02-22 10:16:42 -0300

    função while

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 function tocar_play(idElementoAudio) {
     document.querySelector(idElementoAudio).play();
 }
-
 const listaDeTeclas = document.querySelectorAll('.tecla');
-
 let contador = 0;
 //enquanto
 while (contador < listaDeTeclas.length) {
-    listaDeTeclas[contador].onclick = tocar_play;
-    contador = contador + 1;
-    console.log(contador);
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    //tamplate string
+    const idAudio = '#som_${instrmento}';
+    tecla.onclick = function () {
+        tocar_play(idAudio);
+    }
 }
+contador = contador + 1;
 
 
 
