1b2160a14c7f14e833e6fd5caaa8d81abc1c5923
anderson rafael <franciscoandersonrafael@gmail.com>
2024-02-22 11:48:48 -0300

    programa finalizado com todas as teclas funcionando

--- script.js
+++ script.js
@@ -1,36 +1,39 @@
-function tocar_play(seletorAudio) {
+function tocaSom(seletorAudio) {
     const elemento = document.querySelector(seletorAudio);
 
-    if (elemento != nul && elemento.localName === 'Audio') {
+    if (elemento && elemento.localName === 'audio') {
         elemento.play();
     }
     else {
-        console.log("elemento não existe");
+        //alert('Elemento não encontrado');
+        console.log('Elemento não encontrado ou seletor inválido');
     }
+
 }
+
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-//enquanto
+//para
 for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
-    //tamplate string
-    const idAudio = `#som_${instrumento}`;
+    const idAudio = `#som_${instrumento}`; //template string
+
     tecla.onclick = function () {
-        tocar_play(idAudio);
+        tocaSom(idAudio);
     }
+
     tecla.onkeydown = function (evento) {
-        if (evento.code === 'Space' || 'Enter') {
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
             tecla.classList.add('ativa');
         }
+
     }
+
     tecla.onkeyup = function () {
         tecla.classList.remove('ativa');
     }
 
 }
-
-
-
-
-
